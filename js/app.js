c5cebed777a7e869c55d405b1a91c724836e30ff
Zitle-Nancy <ancizj@hotmail.com>
2017-12-19 19:54:14 -0600

    evalua los posibles errores de resultados

--- js/app.js
+++ js/app.js
@@ -25,12 +25,25 @@
 		valueA = Number(a.value);
 		valueB = Number(b.value); 
 		valueC = Number(c.value);
-		var discriminate = Math.sqrt(Math.pow(valueB, 2) - 4 * (valueA * valueC));
+		var radicando = Math.pow(valueB, 2) - 4 * (valueA * valueC);
+		if(radicando < 0){
+			alert('El resultado de la raiz cuadrada es negativa');
+			x1.innerText = 'No hay solución';
+			x2.innerText = 'No hay solución';
+			return;
+		}
+		if(valueA === 0){
+			alert('Todo numero dividido por cero es un error');
+			x1.innerText = 'Error';
+			x2.innerText = 'Error';
+			return;
+		}
+		var discriminate = Math.sqrt(radicando);
 		var numeratorPositive = (-1) * (valueB) + discriminate;
 		var numeratorNegative = (-1) * (valueB) - discriminate;
 		var denominator = (2 * valueA);
 		var resultX1 = numeratorPositive / denominator;
-		var resultX2 = numeratorNegative / denominator;
+		var resultX2 = numeratorNegative / denominator;	
 		x1.innerText = resultX1;
 		x2.innerText = resultX2;
 	};
