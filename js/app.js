2468c0bdb5d64e2245eb9beaae836e24140561c0
Zitle-Nancy <ancizj@hotmail.com>
2017-12-19 21:38:17 -0600

    esta pensada la funcion para ser utilizada como libreria

--- js/app.js
+++ js/app.js
@@ -1,16 +1,13 @@
 (function(){
 
-	var valueA;
-	var valueB;
-	var valueC;
 	var a = document.getElementById('a');
 	var b = document.getElementById('b');
 	var c = document.getElementById('c');
-	var x1 = document.getElementById('x1');
-	var x2 = document.getElementById('x2');
+	// var x1 = document.getElementById('x1');
+	// var x2 = document.getElementById('x2');
 	var numbers = document.getElementsByClassName('numbers');
 	var btnCalcular = document.getElementById('btn-calcular');
-	btnCalcular.addEventListener('click', secondDegreeEquationResult);
+	btnCalcular.addEventListener('click', calculate);
 	for(var i=0; i < numbers.length; i++){
 		numbers[i].addEventListener('keydown',validateNumber);
 	}
@@ -20,31 +17,45 @@
 			e.preventDefault();
 		}
 	};
-	function secondDegreeEquationResult(e) {
+	function calculate(e) {
 		e.preventDefault();
-		valueA = Number(a.value);
-		valueB = Number(b.value); 
-		valueC = Number(c.value);
+		secondDegreeEquationResult(a,b,c);
+	}
+	function secondDegreeEquationResult(a,b,c) {
+		var valueA = Number(a.value);
+		var valueB = Number(b.value); 
+		var valueC = Number(c.value);
 		var radicando = Math.pow(valueB, 2) - 4 * (valueA * valueC);
-		if(radicando < 0){
-			alert('El resultado de la raiz cuadrada es negativa');
-			x1.innerText = 'No hay solución';
-			x2.innerText = 'No hay solución';
-			return;
-		}
-		if(valueA === 0){
-			alert('Todo numero dividido por cero es un error');
-			x1.innerText = 'Error';
-			x2.innerText = 'Error';
-			return;
+		var result = {
+			x1:"",
+			x2:"",
+			message:""
 		}
 		var discriminate = Math.sqrt(radicando);
 		var numeratorPositive = (-1) * (valueB) + discriminate;
 		var numeratorNegative = (-1) * (valueB) - discriminate;
 		var denominator = (2 * valueA);
-		var resultX1 = numeratorPositive / denominator;
-		var resultX2 = numeratorNegative / denominator;	
-		x1.innerText = resultX1;
-		x2.innerText = resultX2;
+		if(radicando < 0){
+			result.message = 'El resultado de la raiz cuadrada es negativa';
+			result.x1 = 'No hay solución';
+			result.x2 = 'No hay solución';
+			return result;
+		}else {
+			result.message = "Exit";
+			result.x1 = (numeratorPositive / denominator);
+			result.x2 = (numeratorNegative / denominator);
+		}
+		if(valueA === 0){
+			result.message = 'Todo numero dividido por cero es un error';
+			result.x1 = 'Error';
+			result.x2 = 'Error';
+			return result;
+		}else {
+			result.message = "Exit";
+			result.x1 = (numeratorPositive / denominator);
+			result.x2 = (numeratorNegative / denominator);
+		}
+		return result;	
 	};
+	// secondDegreeEquationResult(a,b,c);
 })();
